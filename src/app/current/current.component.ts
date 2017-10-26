c246e784809b0d51e25648ccb16e7d883f2792bc
Stephen Maus <stephen_maus@yahoo.com>
2017-10-25 21:26:02 -0400

    Added bootstrap

--- src/app/current/current.component.ts
+++ src/app/current/current.component.ts
@@ -3,76 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-current',
   templateUrl: './current.component.html',
-  styleUrls: ['./current.component.css'],
-  styles: [`
-    h1 {
-        text-decoration:underline;
-   
-    }
-table {
-    border-collapse: collapse;
-
-}
-
-table, th, td {
-    border: 1px solid black;
-}
-th, td {
-    padding: 15px;
-    text-align: left;
-  
-}
-table {
-    width: 100%;
-}
-
-th {
-    height: 50px;
-  font: 100em;
-  color: white;
-  background-color: #A9A9A9;
-}
-tr:hover {background-color: #f5f5f5}
-
-ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    width: 200px;
-    background-color: #f1f1f1;
-}
-
-li a {
-    display: block;
-    color: #000;
-    padding: 8px 16px;
-    text-decoration: none;
-}
-
-/* Change the link color on hover */
-li a:hover {
-    background-color: #555;
-    color: white;
-}
-.active {
-    background-color: #4CAF50;
-    color: white;
-}
-ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    width: 10%;
-    background-color: #f1f1f1;
-    height: 75%; /* Full height */
-    position: fixed; /* Make it stick, even on scroll */
-    overflow: auto; /* Enable scrolling if the sidenav has too much content */
-}
-.pageContent{
-    margin-left: 12%;
-}
-
-`]
+  styleUrls: ['./current.component.css']
 
 
 })
