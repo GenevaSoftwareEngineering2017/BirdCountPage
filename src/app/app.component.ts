90ff04678b1c0680dbbcb86766c8bf35e0a0d51c
Stephen Maus <stephen_maus@yahoo.com>
2017-11-28 20:19:33 -0500

    Attempt to connect API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,17 @@ import { Component } from '@angular/core';
    
 })
 export class AppComponent {
+//private apiUrl = 'http://localhost:58281/api/test';
+//data: any = {};
 
+//constructor(private http: Http)
+//{
+	//console.log('Hey I think this might actually work!');
+	//this.getData();
+//}
+
+//getData() {
+//return this.http.get(this.apiUrl)
+//.map((res: Response) => res.json())
+//}
 }
