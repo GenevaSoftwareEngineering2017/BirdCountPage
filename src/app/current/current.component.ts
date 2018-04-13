53e09b49a80b889c98636bc288eeee4cd98fc312
Stephen Maus <stephen_maus@yahoo.com>
2018-04-12 20:29:48 -0400

    API Connected to Current Page

--- src/app/current/current.component.ts
+++ src/app/current/current.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Http, Response} from '@angular/http';
+import { URLSearchParams } from '@angular/http';
+import { Headers, RequestOptions} from '@angular/http';
 
 @Component({
   selector: 'app-current',
@@ -9,7 +13,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrentComponent implements OnInit {
 
-  constructor() { }
+  apiUrl ='http://localhost:58281/api/team/getall';
+  data: any = {};
+	   constructor (private http: Http)
+	   {
+		   this.getNames();
+		   this.getData();
+	   }
+
+	   getData()
+	   {
+		   return this.http.get(this.apiUrl)
+		   .map((res: Response) => res.json())
+	   }
+
+	   getNames()
+	   {
+		   this.getData().subscribe(data => {
+		   this.data = data
+		   console.log(data)
+		   })
+	   }
 
   ngOnInit() {
   }
